1fc3bb893e5a6259252b8aba00aca0f4eaae0237
Lukyord <ttansiri524@gmail.com>
2023-02-22 11:54:07 +0100

    add NotSignedIn Page directory

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,12 +1,18 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import Hamgberger from "./components/Hamgberger";
 import NavLinks from "./components/NavLinks";
 
 export default function Header() {
+  const router = useRouter();
+
   return (
     <div className="h-20 md:h-24 w-full bg-purple-700 flex items-center justify-between p-2 px-4 shadow-2xl">
-      <h1 className="text-white text-4xl md:text-6xl font-bold my-auto cursor-pointer">
+      <h1
+        onClick={() => router.replace("/")}
+        className="text-white text-4xl md:text-6xl font-bold my-auto cursor-pointer"
+      >
         VeriFire
       </h1>
       <div>
--- src/Header/components/Navlink.tsx
+++ src/Header/components/Navlink.tsx
@@ -1,13 +1,24 @@
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 type Props = {
   navlink: string;
 };
 
 export default function Navlink({ navlink }: Props) {
+  const router = useRouter();
+
+  const handleSubmit = () => {
+    if (navlink != "Home/About") {
+      router.push(`/${navlink.toLowerCase().replace(/ /g, "_")}`);
+    } else {
+      router.replace("/");
+    }
+  };
+
   return (
-    <Link href={`/${navlink}`} className="text-white font-semibold">
+    <button onClick={handleSubmit} className="text-white font-semibold">
       {navlink}
-    </Link>
+    </button>
   );
 }

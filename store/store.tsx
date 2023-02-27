5e478e31d3e717007ce99e70a5f0cebd1b3f86f2
Lukyord <ttansiri524@gmail.com>
2023-02-27 18:04:34 +0100

    draft useUserStore in store

--- store/store.tsx
+++ store/store.tsx
@@ -1,13 +1,65 @@
 import { create } from "zustand";
+import { User as FirebaseUser } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 
 interface NavigationModalState {
   modalShown: boolean;
   setModalShown: (modalShown: boolean) => void;
 }
 
+interface UserState {
+  user: FirebaseUser | null;
+  signin: (email: string, password: string) => Promise<void>;
+  signup: (email: string, password: string) => Promise<void>;
+  signout: () => Promise<void>;
+}
+
+const auth = getAuth();
+
 const useModalShownStore = create<NavigationModalState>((set) => ({
   modalShown: false,
   setModalShown: () => set((state) => ({ modalShown: !state.modalShown })),
 }));
 
-export default useModalShownStore;
+const useUserStore = create<UserState>((set) => ({
+  user: null,
+  signin: async (email: string, password: string) => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      set({ user: userCredential.user });
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  signup: async (email: string, password: string) => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      set({ user: userCredential.user });
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  signout: async () => {
+    try {
+      await signOut(auth);
+      set({ user: null });
+    } catch (error) {
+      console.error(error);
+    }
+  },
+}));
+
+export default { useModalShownStore };

b4e7eb3ecfbc5ab1db7a6a73371bb5613cf15b9a
Lukyord <ttansiri524@gmail.com>
2023-03-08 19:03:59 +0100

    change nextConfig to upload pic from net

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import ProtectedRoute from "../../middleware/ProtectedRoute";
 import useAuthStore from "../../store/authStore";
@@ -15,6 +16,13 @@ export default function Profile() {
   return (
     <ProtectedRoute>
       <div>Profile</div>
+      <Image
+        src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+        // src="https://images.unsplash.com/photo-1666782199657-09885cc1c088"
+        alt="user profile image"
+        width={20}
+        height={20}
+      />
       <button onClick={() => router.push("/profile/create")}>create</button>
       <button onClick={handleSignOut}>sign out</button>
     </ProtectedRoute>

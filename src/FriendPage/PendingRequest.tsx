507bc9bad9c2d659bc8e8c05ef8591f05cbcc797
Lukyord <ttansiri524@gmail.com>
2023-03-12 12:39:55 +0100

    fix getDocs from pendingFriend Request

--- src/FriendPage/PendingRequest.tsx
+++ src/FriendPage/PendingRequest.tsx
@@ -3,34 +3,36 @@
 import { DocumentData } from "firebase/firestore";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import getPendingRequest from "../../lib/AddandAcceptFriends/getPendingRequest";
 import useAuthStore from "../../store/authStore";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase";
 
 export default function PendingRequest() {
   const { id } = useAuthStore();
   const router = useRouter();
-  const [pendingRequests, setPendingRequests] = useState<
-    DocumentData | undefined | null
-  >([]);
+  const [pendingRequests, setPendingRequests] = useState<DocumentData>([]);
 
   useEffect(() => {
     if (!id) {
       return;
     }
-
-    console.log(id);
-    const requests = getPendingRequest(id);
-    setPendingRequests(requests);
+    async function getPendingRequest() {
+      const pendingReq = await getDocs(
+        collection(db, "users", id, "pendingFriend")
+      );
+      setPendingRequests(pendingReq.docs.map((doc) => doc.data()));
+    }
+    getPendingRequest();
   }, [id]);
 
   return (
     <div>
-      {/* {pendingRequests?.map((friendRequest: DocumentData) => (
-        <div key={friendRequest.id}>
-          <h2>{friendRequest.displayName}</h2>
-          <p>{friendRequest.dob}</p>
+      {pendingRequests.map((data: DocumentData) => (
+        <div key={data.userData.id}>
+          <h2>{data.userData.displayName}</h2>
+          <p>{data.userData.dob}</p>
         </div>
-      ))} */}
+      ))}
       {/* {pendingRequests && pendingRequests[0]} */}
       <button onClick={() => console.log(pendingRequests)}>o</button>
     </div>

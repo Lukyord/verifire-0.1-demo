04e96281fa5ee1eac4744fe2bbb479ff4114c3e7
Lukyord <ttansiri524@gmail.com>
2023-03-13 09:51:50 +0100

    implement can't add yourself as a friend

--- src/FriendPage/PendingRequest.tsx
+++ src/FriendPage/PendingRequest.tsx
@@ -33,27 +33,6 @@ export default function PendingRequest() {
   return (
     <div className={styles.list_box}>
       {pendingRequests.map((data: DocumentData) => (
-        // <div key={data.userData.id} className={styles.list}>
-        //   <div className="flex flex-row gap-1 items-center">
-        //     <Image
-        //       className={`${styles.circular_pic}`}
-        //       src={
-        //         data.userData.photoURL === ""
-        //           ? "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-        //           : data.userData.photoURL
-        //       }
-        //       alt="user profile image"
-        //       width={40}
-        //       height={40}
-        //     />
-        //     <h2>{data.userData.displayName}, </h2>
-        //     <p>{data.userData.dob}</p>
-        //   </div>
-        //   <div className="flex flex-row gap-1 p-2">
-        //     <CheckCircleIcon className="w-8 h-8" color="gray" />
-        //     <XCircleIcon className="w-8 h-8" color="gray" />
-        //   </div>
-        // </div>
         <UserList data={data.userData} key={data.userData.id} />
       ))}
     </div>
---
+++ src/FriendPage/components/UserCard.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import Image from "next/image";
+import stylesCard from "../../../styles/UserCard.module.css";
+import stylesImage from "../../../styles/Image.module.css";
+import { DocumentData } from "firebase/firestore";
+import useAuthStore from "../../../store/authStore";
+import { useRouter } from "next/navigation";
+import sendFriendRequest from "../../../lib/AddandAcceptFriends/sendFriendRequest";
+import { useEffect, useState } from "react";
+
+export default function UserCard({ user }: DocumentData) {
+  const [searchResult, setSearchResult] = useState("");
+  const { id, verifireId } = useAuthStore();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user.verifireId === verifireId) {
+      setSearchResult("yourself");
+    } else {
+      setSearchResult("ok");
+    }
+  }, []);
+
+  async function handleAdd() {
+    sendFriendRequest(user.id, id);
+    router.push("friends");
+  }
+
+  return (
+    <div className={stylesCard.user_card}>
+      <h1>{user.displayName}</h1>
+      <Image
+        className={stylesImage.circular_pic}
+        src={
+          user.photoURL === ""
+            ? "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+            : user.photoURL
+        }
+        alt="user profile image"
+        width={1080}
+        height={1080}
+      />
+      {searchResult === "ok" ? (
+        <button onClick={handleAdd}>Add</button>
+      ) : (
+        <div>
+          {searchResult === "yourself" && <p>Can't add yourself as a friend</p>}
+          {searchResult === "already Friend" && <p>Already friend</p>}
+        </div>
+      )}
+    </div>
+  );
+}

8ad33875602761f11a9e2ac9c72b7a43353dcdef
Lukyord <ttansiri524@gmail.com>
2023-04-17 10:33:19 +0200

    implement dummy email sender using nodemail

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,25 @@
 "use client";
 
 import React from "react";
+import { emerApi } from "../lib/SendingEmerNotification/api";
 import HomePageSignedIn from "../src/HomePage/HomePageSignedIn";
 import useAuthStore from "../store/authStore";
 
 export default function page() {
   const { user } = useAuthStore();
+
+  async function handleClick() {
+    try {
+      await emerApi({
+        emergencyContact1: "0899214213",
+        relationship1: "Dad",
+        emergencyContact2: "0818349831",
+        relationship2: "Mom",
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <>
@@ -16,6 +30,7 @@ export default function page() {
       ) : (
         <div className="">
           <p>Welcome guest</p>
+          <button onClick={handleClick}>make call</button>
         </div>
       )}
     </>
---
+++ lib/SendingEmerNotification/api.ts
@@ -0,0 +1,13 @@
+export async function emerApi(data: EmergencyContact) {
+  fetch("/api/emer", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application.json",
+    },
+  }).then((res) => {
+    if (!res.ok) throw new Error("Failed to send message");
+    return res.json();
+  });
+}
